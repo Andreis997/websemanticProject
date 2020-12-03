889bd6bd089852c42effc9069713f3a13eac6d0b
andreis997 <andreijugaurs@gmail.com>
2020-12-03 21:47:38 +0200

    Add Controllers and Item Model

---
+++ src/main/java/com/hypnos/websemantic/controllers/TeachingToolsController.java
@@ -0,0 +1,31 @@
+package com.hypnos.websemantic.controllers;
+
+import com.hypnos.websemantic.models.Item;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class TeachingToolsController {
+
+    @GetMapping("/list")
+    public String listItems() {
+        return "";
+    }
+
+    public String listSpecificItems(
+            @RequestParam(value = "xDays", defaultValue = "0", required = false) Integer xDays,
+            @RequestParam(value = "category", defaultValue = "", required = false) String category
+    ) {
+        return "";
+    }
+
+    @GetMapping("/item/")
+    public String showItem(@RequestParam String id) {
+        return "";
+    }
+
+    @PostMapping("/item/add")
+    public String addItem(@ModelAttribute Item item) {
+        return "";
+    }
+}
---
+++ src/main/java/com/hypnos/websemantic/models/Item.java
@@ -0,0 +1,8 @@
+package com.hypnos.websemantic.models;
+
+import lombok.Data;
+
+// https://spring.io/guides/gs/handling-form-submission/
+@Data
+public class Item {
+}
